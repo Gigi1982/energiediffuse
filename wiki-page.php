5ca490b75ce071786b009ec52cf5c791f40d998a
Gigi1303 <basile.luigi@gmail.com>
2016-11-07 22:17:24 +0100

    internal and wiki page

---
+++ wiki-page.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: wiki */ get_header(); ?>
+
+<section class="internal-page-block">
+    <header class="internal-page-header" style="background-image:url('<?php echo get_template_directory_uri(); ?>/img/header-internal.jpg')">
+        <h1><?php the_title(); ?></h1>
+    </header>
+    <div class="container">
+        <section class="internal-page-content">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="icon-blocks-container">
+                    <div class="rounded-png">
+                        <img width="198" height="198" src="<?php echo get_template_directory_uri(); ?>/img/icona01-wiki.png">
+                    </div>
+                    <p><span class="subtitles">Operatore dedicato</span></p>
+                    <p>Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo </p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="icon-blocks-container">
+                    <div class="rounded-png">
+                        <img width="198" height="198" src="<?php echo get_template_directory_uri(); ?>/img/icona02-wiki.png">
+                    </div>
+                    <p><span class="subtitles">Analisi consumi</span></p>
+                    <p>Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo </p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="icon-blocks-container">
+                    <div class="rounded-png">
+                        <img width="198" height="198" src="<?php echo get_template_directory_uri(); ?>/img/icona03-wiki.png">
+                    </div>
+                    <p><span class="subtitles">Previsione costi futuri</span></p>
+                    <p>Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo </p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="icon-blocks-container">
+                    <div class="rounded-png">
+                        <img width="198" height="198" src="<?php echo get_template_directory_uri(); ?>/img/icona04-wiki.png">
+                    </div>
+                    <p><span class="subtitles">Autolettura</span></p>
+                    <p>Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo Testo testo testo testo testo testo </p>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
+<?php get_footer(); ?>
